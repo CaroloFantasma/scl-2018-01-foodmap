d6e079c4b2ae8bf51a62922141ce07c65097de85
Carolina Lastra <carolastra.cofre@gmail.com>
2018-07-25 15:27:14 -0400

    ¡Por fin aparece el mapa de Goggle!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,10 @@
-const splashTime = 3;
-const urlMenu = 'file:///C:/Users/CaroloFantasma/Documents/scl-2018-01-foodmap/assets/mainIndex.html';
-setTimeout("window.location = '" + urlMenu + "'", splashTime * 1000);
+// const splashTime = 3;
+// const urlMenu = 'file:///C:/Users/CaroloFantasma/Documents/scl-2018-01-foodmap/assets/mainIndex.html';
+// setTimeout("window.location = '" + urlMenu + "'", splashTime * 1000);
 
-function placesSearch (platform) {
-  var placesService= platform.getPlacesService(),
-    parameters = {
-      at: '37.7942,-122.4070',
-      q: 'restaurant'};
-
-  placesService.search(parameters,
-    function (result) {
-      alert(result);
-    }, function (error) {
-      alert(error);
-    });
+function initMap() {
+  let locationMap = { lat: -33.4569, lng: -70.648 };
+  let map = new google.maps.Map(
+    document.getElementById('mapContainer'), { zoom: 15, center: locationMap });
+  let markerPosition = new google.maps.Marker({ position: locationMap, map: map });
 }
