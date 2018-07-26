00ae02214c6fc15630f5db23e275d5ade447eef4
Carolina Lastra <carolastra.cofre@gmail.com>
2018-07-26 08:05:17 -0400

    input funcionando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,93 @@
-// const splashTime = 3;
-// const urlMenu = 'file:///C:/Users/CaroloFantasma/Documents/scl-2018-01-foodmap/assets/mainIndex.html';
-// setTimeout("window.location = '" + urlMenu + "'", splashTime * 1000);
-
-// Función para que aparezca el mapa de Google
-function initMap() {
-  let locationMap = { lat: -33.400, lng: -70.600 };
-  let map = new google.maps.Map(
-    document.getElementById('mapContainer'), { zoom: 15, center: locationMap });
-  let markerPosition = new google.maps.Marker({ position: locationMap, map: map });
+function screenOne() {
+  document.getElementById('screenOne').style.display = 'block';
+  document.getElementById('screenTwo').style.display = 'none';
 }
 
-// Obtener ubicación (geolocalización)
-var map, infoWindow;
+function screenTwo() {
+  document.getElementById('screenOne').style.display = 'none';
+  document.getElementById('screenTwo').style.display = 'block';
+}
+
+
+window.onload = function() {
+  setTimeout(screenTwo, 4000);
+};
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('mapContainer'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 6
+    center: { lat: -33.400, lng: -70.600 },
+    zoom: 15,
+    mapTypeId: 'roadmap'
   });
   infoWindow = new google.maps.InfoWindow;
 
-  // Try HTML5 geolocation.
+  // Create the search box and link it to the UI element.
+  let input = document.getElementById('searchInput');
+  let searchBox = new google.maps.places.SearchBox(input);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  let markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    let places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    let bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      let icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
+      let pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
 
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      infoWindow.setContent('Ubicación actual');
       infoWindow.open(map);
       map.setCenter(pos);
     }, function() {
@@ -38,43 +97,32 @@ function initMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
-}
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
     'Error: The Geolocation service failed.' :
      'Error: Your browser doesn\'t support geolocation.');
-  infoWindow.open(map);
+infoWindow.open(map);
 }
 
-var map;
-var service;
-var infowindow;
+// let infowindow = new google.maps.InfoWindow();
+//   let service = new google.maps.places.PlacesService(map);
 
-function initialize() {
-  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
-
-  map = new google.maps.Map(document.getElementById('mapContainer'), {
-      center: pyrmont,
-      zoom: 15
-    });
-
-  var request = {
-    location: pyrmont,
-    radius: '500',
-    type: ['restaurant']
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-}
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
-    }
-  }
+//   service.getDetails({
+//     placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
+//   }, function(place, status) {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//       let marker = new google.maps.Marker({
+//         map: map,
+//         position: place.geometry.location
+//       });
+//       google.maps.event.addListener(marker, 'click', function() {
+//         infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+//           'Place ID: ' + place.place_id + '<br>' +
+//           place.formatted_address + '</div>');
+//         infowindow.open(map, this);
+//       });
+//     }
+//   });
 }
