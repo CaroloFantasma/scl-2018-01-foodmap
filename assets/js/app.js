16d4dd60e75ad03f5b0877f606e98b9c5ee57349
Carolina Lastra <carolastra.cofre@gmail.com>
2018-07-26 19:20:58 -0400

    modal no funcionando aún

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,14 +27,14 @@ function initMap() {
   let searchBox = new google.maps.places.SearchBox(input);
 
   // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
+  map.addListener('bounds_changed', function () {
     searchBox.setBounds(map.getBounds());
   });
 
   let markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
-  searchBox.addListener('places_changed', function() {
+  searchBox.addListener('places_changed', function () {
     let places = searchBox.getPlaces();
 
     if (places.length == 0) {
@@ -42,14 +42,14 @@ function initMap() {
     }
 
     // Clear out the old markers.
-    markers.forEach(function(marker) {
+    markers.forEach(function (marker) {
       marker.setMap(null);
     });
     markers = [];
 
     // For each place, get the icon, name and location.
     let bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
+    places.forEach(function (place) {
       if (!place.geometry) {
         console.log("Returned place contains no geometry");
         return;
@@ -69,7 +69,7 @@ function initMap() {
         title: place.name,
         position: place.geometry.location
       }));
-      google.maps.event.addListener(markers, 'click', function() {
+      google.maps.event.addListener(markers, 'click', function () {
         infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
           'Place ID: ' + place.place_id + '<br>' +
           place.formatted_address + '</div>');
@@ -85,9 +85,10 @@ function initMap() {
     });
     map.fitBounds(bounds);
   });
+
   //Ubicación actual (geolocalización)
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
       let pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
@@ -97,7 +98,7 @@ function initMap() {
       infoWindow.setContent('Ubicación actual');
       infoWindow.open(map);
       map.setCenter(pos);
-    }, function() {
+    }, function () {
       handleLocationError(true, infoWindow, map.getCenter());
     });
   } else {
@@ -105,66 +106,33 @@ function initMap() {
     handleLocationError(false, infoWindow, map.getCenter());
   }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-    'Error: The Geolocation service failed.' :
-     'Error: Your browser doesn\'t support geolocation.');
-infoWindow.open(map);
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 
-var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-    title: 'Uluru (Ayers Rock)'
-  });
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
-}
+  }
 
 
-// let infowindow = new google.maps.InfoWindow();
-//   let service = new google.maps.places.PlacesService(map);
+  // let infowindow = new google.maps.InfoWindow();
+  //   let service = new google.maps.places.PlacesService(map);
 
-//   service.getDetails({
-//     placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
-//   }, function(place, status) {
-//     if (status === google.maps.places.PlacesServiceStatus.OK) {
-//       let marker = new google.maps.Marker({
-//         map: map,
-//         position: place.geometry.location
-//       });
-//       google.maps.event.addListener(marker, 'click', function() {
-//         infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-//           'Place ID: ' + place.place_id + '<br>' +
-//           place.formatted_address + '</div>');
-//         infowindow.open(map, this);
-//       });
-//     }
-//   });
+  //   service.getDetails({
+  //     placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
+  //   }, function(place, status) {
+  //     if (status === google.maps.places.PlacesServiceStatus.OK) {
+  //       let marker = new google.maps.Marker({
+  //         map: map,
+  //         position: place.geometry.location
+  //       });
+  //       google.maps.event.addListener(marker, 'click', function() {
+  //         infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+  //           'Place ID: ' + place.place_id + '<br>' +
+  //           place.formatted_address + '</div>');
+  //         infowindow.open(map, this);
+  //       });
+  //     }
+  //   });
 }
